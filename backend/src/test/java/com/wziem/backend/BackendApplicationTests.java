261865d1eff6b67c10c5395fe6075ace4448eaed
w-ziem <ziembowiczwojciech@gmail.com>
2025-08-05 23:04:49 +0200

    Initial commit front/back end

---
+++ backend/src/test/java/com/wziem/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wziem.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
