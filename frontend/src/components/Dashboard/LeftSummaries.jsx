a5bc414c7075197ae0d020fcd44e09b2491d5713
w-ziem <ziembowiczwojciech@gmail.com>
2025-09-15 15:29:31 +0200

    Add welcome message to left summaries on left dashboard

--- frontend/src/components/Dashboard/LeftSummaries.jsx
+++ frontend/src/components/Dashboard/LeftSummaries.jsx
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react';
-import {jwtDecode} from "jwt-decode";
 import QuickLinks from "./QuickLinks.jsx";
 import {useAuth} from "../../contexts/AuthProvider.jsx";
 import AddStudentForm from "../Forms/AddStudentForm.jsx";
@@ -13,8 +12,13 @@ const LeftSummaries = () => {
     }, [user]);
 
     return (
-        <div className="w-[60%] h-screen p-10">
-            <h1 className="gradient-text drop-shadow-2xl drop-shadow-secondary text-5xl/18 line-2 font-semibold">Cześć, {name}</h1>
+        <div className="w-[60%] p-10">
+            <div className="ml-10">
+                <h1 className="gradient-text drop-shadow-2xl drop-shadow-secondary text-5xl/18 line-2 font-semibold">Cześć, {name}</h1>
+                <p className="text-xl text-text/70 font-medium">
+                    Witaj z powrotem w swoim panelu tutora
+                </p>
+            </div>
             <QuickLinks />
             <LatestLessons />
         </div>
--- frontend/src/pages/TutorDashboardPage.jsx
+++ frontend/src/pages/TutorDashboardPage.jsx
@@ -3,15 +3,17 @@ import LeftSummaries from "../components/Dashboard/LeftSummaries.jsx";
 import RightSummaries from "../components/Dashboard/RightSummaries.jsx";
 
 const TutorDashboardPage = () => {
-
     return (
-        <section className="w-full h-screen flex gap-5 overflow-hidden bg-white/40 backdrop-blur-xl">
-            <div className="absolute w-[900px] h-[300px] bg-secondary/70 rounded-[20px] blur-[250px] bottom-1/5 right-20 -z-10"></div>
-            <div className="absolute w-[400px] h-[200px] bg-fuchsia-500/80 rounded-full blur-[200px] bottom-50 left-30 -z-10"></div>
-            <div className="absolute w-[300px] h-[100px] bg-yellow-300 rounded-full blur-[100px] top-20 right-30 -z-10"></div>
-            <LeftSummaries />
-            <RightSummaries />
-        </section>
+        <div className="relative min-h-screen overflow-hidden">
+            <div className="absolute w-[900px] h-[300px] bg-secondary/50 rounded-[20px] blur-[250px] bottom-1/5 right-20 -z-10"></div>
+            <div className="absolute w-[400px] h-[200px] bg-fuchsia-500/50 rounded-full blur-[200px] bottom-50 left-30 -z-10"></div>
+            <div className="absolute w-[300px] h-[100px] bg-yellow-300/80 rounded-full blur-[100px] top-20 right-30 -z-10"></div>
+
+            <section className="relative z-10 w-full min-h-screen flex gap-8 bg-white/[0.02] backdrop-blur-[1px]">
+                <LeftSummaries />
+                <RightSummaries />
+            </section>
+        </div>
     );
 };
 
