c3309f760bf3f11deff65d027d5b14ebe5ba8b5d
w-ziem <ziembowiczwojciech@gmail.com>
2025-09-30 13:05:34 +0200

    add missing WebhookController.java file

--- backend/src/main/java/com/wziem/backend/controllers/WebhookController.java
+++ backend/src/main/java/com/wziem/backend/controllers/WebhookController.java
@@ -1,15 +1,9 @@
 package com.wziem.backend.controllers;
 
-import com.stripe.Stripe;
-import com.stripe.exception.SignatureVerificationException;
-import com.stripe.model.PaymentIntent;
-import com.stripe.net.Webhook;
+import com.wziem.backend.dtos.PaymentStatusObject;
 import com.wziem.backend.services.LessonService;
 import com.wziem.backend.services.StripePaymentGateway;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -21,9 +15,11 @@ import java.util.Map;
 @RestController
 public class WebhookController {
     private final StripePaymentGateway stripePaymentGateway;
+    private final LessonService lessonService;
 
     @PostMapping("/webhook")
     public void handleWebhook(@RequestHeader Map<String, String> headers, @RequestBody String payload) {
-        stripePaymentGateway.handleWebhookEvent(headers, payload);
+        PaymentStatusObject data = stripePaymentGateway.handleWebhookEvent(headers, payload);
+        lessonService.handlePaymentStatus(data);
     }
 }
